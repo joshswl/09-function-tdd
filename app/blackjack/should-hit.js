bc5e41ef256904065cfabd9c5480e742d0f6f258
Josh Lee <joshswl@gmail.com>
2017-02-13 17:11:47 -0600

    should hit complete

--- app/blackjack/should-hit.js
+++ app/blackjack/should-hit.js
@@ -1,3 +1,9 @@
 import cardSum from './card-sum';
 
-export default function () {}
+export default function (value1, value2) {
+  if(value1 === value2) {
+    return false;
+  }
+
+  return cardSum(value1, value2) <= 17;
+}
