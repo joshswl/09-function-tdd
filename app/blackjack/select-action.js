95341ebeb1bf8d70116d04a1468da61805814b96
Josh Lee <joshswl@gmail.com>
2017-02-13 20:56:58 -0600

    same first and deep equal for array complete

--- app/blackjack/select-action.js
+++ app/blackjack/select-action.js
@@ -1,4 +1,14 @@
 import shouldSplit from './should-split';
 import shouldHit from './should-hit';
 
-export default function () {}
+export default function (value1, value2) {
+  if(shouldSplit(value1, value2)) {
+    return 'split';
+  }
+
+  if (shouldHit(value1, value2)) {
+    return 'hit';
+  }
+
+  return 'stay';
+}
