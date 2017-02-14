d03ec30fcdbb6f165abea9200a1b2189262c43ff
Josh Lee <joshswl@gmail.com>
2017-02-14 08:58:54 -0600

    alive neighbors complete

--- app/conway/alive-neighbors.js
+++ app/conway/alive-neighbors.js
@@ -1,1 +1,12 @@
-export default function () {}
+export default function (arr) {
+
+  let aliveNeighbors= 0;
+
+for(let i = 0; i <= arr.length; i++) {
+    if (arr[i]) {
+      aliveNeighbors += 1;
+    }
+  }
+
+  return aliveNeighbors;
+}
